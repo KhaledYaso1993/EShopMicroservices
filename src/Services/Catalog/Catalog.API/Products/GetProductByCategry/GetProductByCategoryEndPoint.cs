d0ab9db959443a7d5fa61df14c463b6f510d28db
Khalid AL-Hihi <KhalidH@leading-point.com>
2025-01-24 18:51:54 +0400

    24\1

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategry/GetProductByCategoryEndPoint.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+//public record GetProductByCategryRequest();
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+public class GetProductByCategoryndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Products/Category/{Category}", async (string Category,ISender sender) => {
+            var result = await sender.Send(new GetProductByCategoryQuery(Category));
+            var response = result.Adapt<GetProductByCategoryResponse>();
+            return Results.Ok(response);
+        }).WithName("GetProcuctByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithDescription("Get Procuct By Category")
+        .WithSummary("Get Procuct By Category"); ;    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategry/GetProductByCategoryHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+
+public record GetProductByCategoryQuery(string category) : IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryHandler(IDocumentSession session) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query
+        , CancellationToken cancellationToken)
+    {
+        var product = await session.Query<Product>().Where(x => x.Category.Contains(query.category)).ToListAsync(cancellationToken);
+        return new GetProductByCategoryResult(product);
+    }
+}
